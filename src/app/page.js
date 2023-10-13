bf32f79fb9346e3bf07da426bc0242535394874a
Prashant <84010844+PrashantFarne@users.noreply.github.com>
2023-10-13 18:12:00 +0530

    Create a project with redux tool kit and Nextjs
    Technology used: 
    - Nextjs
    - Redux Toolkit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import AddUsers from './components/AddUsers'
+import DisplayUsers from './components/DisplayUsers'
+
+const Home = () => {
+  return (
+    <main className='main'>
+      <AddUsers />
+      <DisplayUsers />
+    </main>
+  )
+}
+
+export default Home;
